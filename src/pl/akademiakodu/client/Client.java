e120e169baa65a90b57dfe0c11a7a8e3ecc12a9f
vallerboy <oskarbank@gmail.com>
2016-11-02 19:30:57 +0100

    zc

---
+++ src/pl/akademiakodu/client/Client.java
@@ -0,0 +1,75 @@
+package pl.akademiakodu.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Client {
+
+	public static final String HOST = "localhost";
+	public static final int PORT = 4122;
+
+	private Socket socket;
+	private ExecutorService threadService;
+
+	public static void main(String[] args) {
+         new Client();
+	}
+
+	public Client() {
+
+		try {
+			socket = new Socket(HOST, PORT);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Uda³o siê nazwi¹zaæ po³¹czenie z " + HOST + ":" + PORT);
+
+		threadService = Executors.newSingleThreadExecutor();
+		readMessages();
+	}
+
+	private void readMessages() {
+
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+					while (true) {
+
+						String message = in.readLine();
+						if (message != null) {
+                             System.out.println("Wiadomoœæ od servera: " + message);
+                         	in.close();
+    						socket.close();
+    						break;
+
+						}
+						
+						// Zamkniêcie
+					
+
+					}
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		};
+		
+		threadService.execute(runnable);
+
+	}
+
+}
---
+++ src/pl/akademiakodu/server/Server.java
@@ -0,0 +1,66 @@
+package pl.akademiakodu.server;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+	public static final String HOST = "localhost";
+	public static final int PORT = 4122;
+	private ServerSocket server;
+
+	private ExecutorService threadService;
+
+	public static void main(String[] args) {
+
+		 new Server();
+
+	}
+
+	public Server() {
+		// tworzymy nowy serwer
+		InetSocketAddress adress = new InetSocketAddress(HOST, PORT);
+		try {
+			server = new ServerSocket();
+			server.bind(adress);
+		} catch (IOException e) {
+			System.out.println("B³¹d podczas inicjalizacji serwera");
+			e.printStackTrace();
+		}
+
+		threadService = Executors.newFixedThreadPool(20);
+		listenToSockets();
+	}
+
+	private void listenToSockets() {
+
+		System.out.println("~Rozpoczynam nas³uchiwanie~");
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+
+				while (true) {
+					try {
+						Socket socket = server.accept();
+						PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+						writer.println("Witaj przyjacielu!");
+						writer.close();
+						socket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+		};
+
+		threadService.execute(runnable);
+	}
+
+}
